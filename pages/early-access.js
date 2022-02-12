eee7d63c031b2956b169de2f1ad680b5ec62dc37
Sharad Chand <pepsighan@sharadchand.com>
2022-02-12 12:47:48 +0545

    Style the page a bit

--- pages/early-access.js
+++ pages/early-access.js
@@ -1,5 +1,5 @@
 import { NextSeo } from "next-seo";
-import { GlobalStyles } from "@mui/material";
+import { Box, GlobalStyles } from "@mui/material";
 import gappe from "assets/gappe.png";
 
 export default function EarlyAccess() {
@@ -21,18 +21,14 @@ export default function EarlyAccess() {
             overflow: hidden;
           }
           
-          iframe {
-            position: absolute; 
-            top: 0; 
-            right: 0; 
-            bottom: 0; 
-            left: 0; 
-            border: 0;
+          body {
+            background-color: #EAE5FE;
           }
         `}
       />
 
-      <iframe
+      <Box
+        component="iframe"
         src="https://tally.so/r/w8goo3?transparentBackground=1"
         width="100%"
         height="100%"
@@ -40,6 +36,14 @@ export default function EarlyAccess() {
         marginHeight="0"
         marginWidth="0"
         title="Want to get early access?"
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          border: 0,
+        }}
       />
     </>
   );
