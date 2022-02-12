dda835f2ba1b13844e940bced4ffd9c58b6a2413
Sharad Chand <pepsighan@sharadchand.com>
2022-02-12 10:58:59 +0545

    Add material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,14 @@
+import Head from "next/head";
+import { CssBaseline } from "@mui/material";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
